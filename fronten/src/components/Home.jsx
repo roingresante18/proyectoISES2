32f1d9d7c5ce2c0800d6ca66a6d05a81376d97f8
mrkondu <joelcabrera3k@gmail.com>
2023-10-13 12:11:06 -0300

    navbar en home y login

--- fronten/src/components/Home.jsx
+++ fronten/src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import Navegador from "./Navegador";
 const Home = () => {
   return (
-    <div>
-      Home de la pagina
-    </div>
-  )
-}
+    <>
+      <Navegador/>
+      <div>Home de la pagina</div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- fronten/src/components/Login.jsx
+++ fronten/src/components/Login.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+// import React from 'react'
 // import Box from '@mui/material/Box';
-
+import Navegador from './Navegador'
 
 const Login = () => {
   return (
     <>
-    
-login de la pagina
+      <Navegador/>
+      Navegador <br />
+      login de la pagina
 
     
     </>
   )
 }
 
-      export default Login
+export default Login
